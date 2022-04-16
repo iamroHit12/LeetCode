24dc577e537b4622af9f7f37a0181d094c10b6a8
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2022-04-16 18:48:05 +0530

    Time: 6 ms (54.65%), Space: 9.7 MB (23.87%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -3,10 +3,8 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         auto it = lower_bound(nums.begin(),nums.end(),target);
         int index = it-nums.begin();
-        if(*it == target){
-            return index;
-        }else{
-            return index;
-        }
+        
+        
+        return index;
     }
 };
