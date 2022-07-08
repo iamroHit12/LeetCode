a155ddc18d4b375a2ebf033f985833d1d29d96e9
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2022-07-08 14:10:57 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.21%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -11,16 +11,11 @@ public:
                 swap(&matrix[i][j],&matrix[j][i]);
             }
         }
-        // for(int i=0;i<matrix.size();i++){
-        //     for(int j=0;j<matrix[i].size();j++){
-        //         cout<<matrix[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        int n = matrix.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
-                swap(&matrix[i][j],&matrix[i][n-j-1]);
+        
+        
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size()/2;j++){
+                swap(&matrix[i][j],&matrix[i][matrix.size()-j-1]);
             }
         }
     }
