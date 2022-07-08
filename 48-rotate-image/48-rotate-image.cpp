394bdae098c8ccfdeb83c27587c50b8a7f9e29b8
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2022-07-08 14:13:03 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.21%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    // void swap(int *x,int *y){
-    //     int temp = *x;
-    //     *x = *y;
-    //     *y = temp;
-    // }
-    
     void swap(int &x,int &y){
         int temp = x;
         x = y;
