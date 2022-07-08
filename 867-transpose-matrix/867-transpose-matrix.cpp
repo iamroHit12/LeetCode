9d9d78ac133d7534a993eb29ec249e468d9664ca
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2022-07-08 12:05:35 +0530

    Time: 15 ms (74.02%), Space: 10.7 MB (40.67%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        cout<<n<<"*"<<m;
+        
         vector<vector<int>> res(m,vector<int>(n,0));
         
         for(int i=0;i<m;i++){
